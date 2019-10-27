41aea60faee5f90f45f2e101ce0239584f48bb91
wangxinyue <wangxinyue@weiboyi.com>
2019-10-27 23:01:42 +0800

    添加extend

--- src/utils/create.js
+++ src/utils/create.js
@@ -6,21 +6,36 @@ import Vue from 'vue'
 export default function create(Component, props) {
     //1.创建出入组件实例
     //Vue.extend({})
-    const vm = new Vue({
-        render(h) {//h即是createElement(tag,data,children)
-            //返回熟知的虚拟dom
-            return h(Component, { props })
-        }
-    }).$mount()//只挂载,不设置宿主,执行初始化过程,没有dom操作
-    document.body.appendChild(vm.$el)
-    //获取组件实例
-    const comp = vm.$children[0];
-    //附加一个删除方法
+    /** const vm = new Vue({
+         render(h) {//h即是createElement(tag,data,children)
+             //返回熟知的虚拟dom
+             return h(Component, { props })
+         }
+     }).$mount()//只挂载,不设置宿主,执行初始化过程,没有dom操作
+     document.body.appendChild(vm.$el)
+     //获取组件实例
+     const comp = vm.$children[0];
+     //附加一个删除方法
+     comp.remove = () => {
+         //移除dom
+         document.body.removeChild(vm.$el)
+         //销毁组件
+         vm.$destroy()
+     }
+     return comp
+     */
+    Object.keys(props).forEach(v => {
+        Component.props[v].default = props[v]
+    })
+
+    const Comp = Vue.extend(Component)
+    const comp = new Comp()
+    comp.$mount()
+    document.body.appendChild(comp.$el)
+
     comp.remove = () => {
-        //移除dom
-        document.body.removeChild(vm.$el)
-        //销毁组件
-        vm.$destroy()
+        document.body.removeChild(comp.$el)
+        comp.$destroy()
     }
     return comp
 }
